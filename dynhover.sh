2d82c63e70e41393ff1731ce9dfae0bc87c1894c
Mark Downey <mark.downey@gmail.com>
2016-05-12 12:19:48 -0400

    Making compatible with launchd

--- dynhover.sh
+++ dynhover.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-[[ $# -lt 3 ]] && echo "Usage: $0 USERNAME PASSWORD DNS_ID"
-
-USERNAME=${1}
-PASSWORD=${2}
-DNS_ID=${3}
+dt=$(date)
+#[[ $# -lt 3 ]] && echo "$dt Usage: $0 USERNAME PASSWORD DNS_ID";exit;
+#
+#USERNAME=${1}
+#PASSWORD=${2}
+#DNS_ID=${3}
+#
+#echo ${1}
 
 # find your DNS ID here: https://www.hover.com/api/domains/yourdomain.com/dns/
 
 # (replace "yourdomain.com" with your actual domain, and look for the record
 # you want to change. The ID looks like: dns1234567)
 
-IP=$(curl "http://ifconfig.me/ip" -s)
+IP=$(curl "http://v4.ifconfig.co" -s)
+
+echo "$dt Updating ${DNS_ID}"
 
 curl "https://www.hover.com/api/dns/${DNS_ID}" \
      -X PUT            \
